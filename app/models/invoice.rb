f97b7f5d5b3c6da1496f184ffa51e59185b1d161
Chee Lee <cheeleertr@gmail.com>
2024-06-09 13:21:54 -0500

    merchant_discounted_revenue method added to invoices

--- app/models/invoice.rb
+++ app/models/invoice.rb
@@ -14,6 +14,28 @@ class Invoice < ApplicationRecord
     invoice_items.sum("unit_price * quantity")
   end
 
+  def merchant_discounted_revenue(merchant_id)
+    @merchant_id = merchant_id
+
+    @highest_discounts = self.invoice_items
+      .joins(item: { merchant: :bulk_discounts })
+      .where('invoice_items.quantity >= bulk_discounts.quantity_threshold and bulk_discounts.merchant_id = ?', @merchant_id)
+      .select('invoice_items.id as invoice_item_id, max(bulk_discounts.percent_discount) as max_discount, merchants.id as merchants_id')
+      .group('invoice_items.id', "merchants.id, items.id")
+
+    self.invoice_items
+      .joins(:item)
+      .joins("left join (#{@highest_discounts.to_sql}) as highest_discounts on highest_discounts.invoice_item_id = invoice_items.id")
+    #ask how is alias items_invoice_items
+      .where("items_invoice_items.merchant_id = ?", @merchant_id)
+      .group('invoice_items.invoice_id')
+      .pluck(
+        Arel.sql(
+          'sum((case when highest_discounts.max_discount is null then 1 else (1 - highest_discounts.max_discount / 100.0) end) * invoice_items.unit_price * invoice_items.quantity)'
+        ))
+      .first
+  end
+
   def total_discounted_revenue
     @highest_discounts = self.invoice_items
       .joins(item: { merchant: :bulk_discounts })
@@ -22,13 +44,11 @@ class Invoice < ApplicationRecord
       .group('invoice_items.id')
       
     invoice_items.joins("left join (#{@highest_discounts.to_sql}) as highest_discounts on highest_discounts.invoice_item_id = invoice_items.id")
-      .where(invoice_id: self.id)
       .group('invoice_items.invoice_id')
       .pluck(
         Arel.sql(
           'sum((case when highest_discounts.max_discount is null then 1 else (1 - highest_discounts.max_discount / 100.0) end) * invoice_items.unit_price * invoice_items.quantity)'
-        )
-      )
+        ))
       .first
     # pry
     #sql that works
@@ -57,6 +77,5 @@ class Invoice < ApplicationRecord
 
     #possibly working return array of invoiceitem
     # @sub_table = invoice_items.joins(item: {merchant: :bulk_discounts}).select("invoice_items.*, max(bulk_discounts.percent_discount) as max_discount, max(bulk_discounts.quantity_threshold) as max_threshold, sum(invoice_items.unit_price * quantity) as base_revenue").where("invoice_items.quantity >= bulk_discounts.quantity_threshold").group("invoice_id, invoice_items.id")
-
   end
 end
